34570a3b19cbd2d166f3e19b4602ccaaa0541b57
30006830 <30006830@student.toiohomai.ac.nz>
2021-09-13 17:47:42 +1200

    commit

---
+++ mapApplication/mapApplication/mapApplication/ViewModels/UserDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace mapApplication
+{    
+    public class UserDetails
+    {
+        private string fname;
+        private string email;
+        private string password;
+        private string bio;
+
+    }
+}
